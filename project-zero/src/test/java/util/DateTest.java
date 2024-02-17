cba2938d438dce4fa270e9d78296c3047d9941a9
a.abolghasemi <a.abolghasemi@digikala.com>
2024-02-17 10:01:04 +0330

    adding test case for comparable date

--- project-zero/src/test/java/util/DateTest.java
+++ project-zero/src/test/java/util/DateTest.java
@@ -24,4 +24,14 @@ public class DateTest extends TestCase {
         assertEquals("Invalid month for next day", nextDay.getMonth(), 11);
         assertEquals("Invalid year for next day", nextDay.getYear(), 1402);
     }
+
+    public void testComparableDate()
+    {
+        Date date1 = new Date(18, 11, 1402);
+        Date date2 = new Date(19, 10, 1402);
+
+        int compareResult = date1.compareTo(date2);
+
+        assertTrue("date 1 should be greater than date 2", compareResult > 0);
+    }
 }
