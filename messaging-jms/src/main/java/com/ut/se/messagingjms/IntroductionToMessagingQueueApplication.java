a2fc05157659f32a92b01e47fb6bec44776450cb
a.abolghasemi <a.abolghasemi@digikala.com>
2024-02-23 01:03:17 +0330

    initialling project

---
+++ messaging-jms/src/main/java/com/ut/se/messagingjms/IntroductionToMessagingQueueApplication.java
@@ -0,0 +1,13 @@
+package com.ut.se.messagingjms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroductionToMessagingQueueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntroductionToMessagingQueueApplication.class, args);
+	}
+
+}
---
+++ messaging-jms/src/test/java/com/ut/se/messagingjms/IntroductionToMessagingQueueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ut.se.messagingjms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntroductionToMessagingQueueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
