a98a55caa2036e6b0931fb70300abd4731553014
a.abolghasemi <a.abolghasemi@digikala.com>
2024-02-24 13:13:57 +0330

    remove redundant import

--- messaging-jms/src/main/java/com/ut/se/messagingjms/Application.java
+++ messaging-jms/src/main/java/com/ut/se/messagingjms/Application.java
@@ -1,6 +1,5 @@
 package com.ut.se.messagingjms;
 
-import com.ut.se.messagingjms.config.JmsConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
