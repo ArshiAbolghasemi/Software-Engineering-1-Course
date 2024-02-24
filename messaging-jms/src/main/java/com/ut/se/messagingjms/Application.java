72502a605c3893f8a349947733878b95161da966
a.abolghasemi <a.abolghasemi@digikala.com>
2024-02-24 13:13:31 +0330

    adding receiving command from queue

--- messaging-jms/src/main/java/com/ut/se/messagingjms/Application.java
+++ messaging-jms/src/main/java/com/ut/se/messagingjms/Application.java
@@ -1,13 +1,18 @@
 package com.ut.se.messagingjms;
 
+import com.ut.se.messagingjms.config.JmsConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.jms.core.JmsTemplate;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+		ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
+		JmsTemplate jmsTemplate = context.getBean(JmsTemplate.class);
+		jmsTemplate.receive();
+ 	}
 
 }
